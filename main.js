41308bc072e0ca48bf7a8773cc25fa42808327f3
Daisy Nardella <daisy50@live.it>
2025-02-18 21:41:53 +0100

    CompletatoCondizioni2

---
+++ main.js
@@ -0,0 +1,39 @@
+//if/else if/else
+
+let temperatura = +prompt('Inserisci la temperatura');
+
+if(temperatura < 20){
+    console.log('Non ci sono più le mezze stagioni'); 
+}else if(temperatura < 30){
+    console.log('mi dia una Peroni sudata');
+}else if(temperatura < 0){
+    console.log("non è tanto il freddo quanto l'umidità");
+}else if(temperatura < -10){
+    console.log('copriti...ancora ti raffreddi');
+}else{
+    console.log('lu mare, lu sole, lu ientu');
+}
+
+//switch
+
+let temperature = +prompt('Inserisci la temperatura');
+let descrizione;
+switch (true) {
+    case temperature <= -10:
+        descrizione = 'copriti...ancora ti raffreddi'
+        break;
+        case temperature <= 0:
+        descrizione = "non è tanto il freddo quanto l'umidità"
+        break;
+        case temperature <= 20:
+        descrizione = 'Non ci sono più le mezze stagioni'
+        break;
+        case temperature <= 30:
+        descrizione = 'mi dia una Peroni sudata'
+        break;
+    default:
+        descrizione = 'lu mare, lu sole, lu ientu'
+}
+
+    console.log(descrizione);
+    
